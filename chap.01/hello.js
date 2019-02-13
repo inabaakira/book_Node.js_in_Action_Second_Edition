64ae761e9d6cd4c4a5af0bb07a258f2717b8decd
INABA Akira <ainaba@idcf.jp>
2019-02-13 18:27:10 +0900

    [Chap.01] 1.4.2 The core modules: hello.js を変更。

--- chap.01/hello.js
+++ chap.01/hello.js
@@ -1,1 +1,10 @@
-console.log("hello from Node");
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello, world.\n");
+});
+
+server.listen(port, () => {
+  console.log('Server listening on: http://localhost:%s', port);
+});
